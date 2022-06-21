28e78b4b264513ebf3d3ace48b8037c34437c258
idanPersi <idan_perski@hotmail.com>
2022-06-22 02:36:26 +0300

    sticky navBar

--- js/Navbar.js
+++ js/Navbar.js
@@ -1,10 +1,10 @@
+"use strict"
+
+
 const navSlide = () => {
 
     const burger = document.querySelector('.burger');
-
     const nav = document.querySelector('.navList-m');
-
-
 
     burger.addEventListener('click', () => {
 nav.classList.toggle('navActive');
@@ -16,3 +16,24 @@ nav.classList.toggle('navActive');
 navSlide();
 
 
+const logo = document.getElementById("logo");
+const navBar = document.getElementsByTagName("nav");
+let sticky = navBar.offssetTop;
+
+// logo.scroll
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  
+    document.getElementById("logo").style.height = "5.5vh";
+    document.getElementById("logo").style.transition = "transform 1s ease-in-out";
+  
+  } else {
+    
+    document.getElementById("logo").style.height = "8.8vh";
+     navbar.classList.remove("sticky");
+  }
+
+}
