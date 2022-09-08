3730b9edfb7047233329b66803d970d3b6b70419
idanPersi <idan_perski@hotmail.com>
2022-09-08 09:55:14 +0300

    navbar indentation

--- js/Navbar.js
+++ js/Navbar.js
@@ -2,13 +2,10 @@
 
 
 const navSlide = () => {
-
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navList-m');
-
-    burger.addEventListener('click', () => {
-nav.classList.toggle('navActive');
-
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.navList-m');
+  burger.addEventListener('click', () => {
+  nav.classList.toggle('navActive');
     });
 
 }
@@ -27,53 +24,13 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   let smallScreen = window.matchMedia("(max-width: 768px)");
   let logoSize =document.getElementById("logo").style.height;
-
-
-  if     (smallScreen.matches){
+  if (smallScreen.matches){
       document.getElementById("logo").style.height= "12vh" //logoSize not working (WHY?!)
     }
   else {if  (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-
           document.getElementById("logo").style.height = "50%"; 
-
           } 
-          
         else {logo.style.height = "100%";}
-         
-   
-  //  logo.style.transform = "transition: transform 1s ease-in-out;"
-    // logo.style.transform = " translate(25%)";
-  
   }
 
 }
-
-
-
-
-// function scrollFunction() {
-//   let smallScreen = window.matchMedia("(max-width: 768px)");
-//   let logoSize =document.getElementById("logo").style.height;
-
-
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//   console.log(logoSize)
-//     document.getElementById("logo").style.height = "50%"; //logoSize not working ()
-
-//      if  (smallScreen.matches){
-//       document.getElementById("logo").style.height= "12vh"
-//     }
-   
-//    logo.style.transform = "transition: transform 1s ease-in-out;"
-//     // logo.style.transform = " translate(25%)";
-  
-//   } else {
-    
-//   logo.style.height = "100%";
-    
-//      logo.style.transform = "transition: transform 3s ease-in-out;"
-//     // logo.style.transform = " translate(25%)";
-//     //  navbar.classList.remove("sticky");
-//   }
-
-// }
